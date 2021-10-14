bce9e7400c87a0b6854d18aa5936dde8feb49929
ArtemEfimovich <artemfender@gmail.com>
2021-10-15 00:00:51 +0300

    Updates

---
+++ precache-manifest.d799b384008c67fd9c826e377c406b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f13ccc42f1d15f3980fa5f8103db8e",
+    "url": "/home_work_ignat/index.html"
+  },
+  {
+    "revision": "8e45a86d1ed11201ba82",
+    "url": "/home_work_ignat/static/css/main.07d96ae1.chunk.css"
+  },
+  {
+    "revision": "2151b80ee14d11b19f40",
+    "url": "/home_work_ignat/static/js/2.0bf31a08.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/home_work_ignat/static/js/2.0bf31a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e45a86d1ed11201ba82",
+    "url": "/home_work_ignat/static/js/main.6f535d33.chunk.js"
+  },
+  {
+    "revision": "83e1f758f76e2d07d0d9",
+    "url": "/home_work_ignat/static/js/runtime-main.38e8da03.js"
+  }
+]);
