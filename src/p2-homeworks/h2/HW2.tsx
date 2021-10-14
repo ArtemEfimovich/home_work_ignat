73627113a632f037d79d1b78650a662a05e76b56
ArtemEfimovich <artemfender@gmail.com>
2021-10-14 21:27:06 +0300

    HW 3

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -25,7 +25,7 @@ export const filterAffairs = (affairs:AffairType[], filter: FilterType ): Affair
     else return affairs.filter(a=>a.priority === filter)
 }
 export const deleteAffair = (affairs: AffairType[], _id:number): AffairType[] => { // need to fix any
-    return affairs.filter(a=>a._id === _id)
+    return affairs.filter(a=>a._id !== _id)
 }
 
 function HW2() {
